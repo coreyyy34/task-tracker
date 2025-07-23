a5ecbbae4e3fc1e8cdaf1d40a705b5fb82fe2280
Corey <cbh344@outlook.com>
2025-07-23 19:58:15 +1200

    add proper input types and placerholders to time entry form, update input label to be smaller

--- src/components/home/time-entries/time-entry-form-dialog.tsx
+++ src/components/home/time-entries/time-entry-form-dialog.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/common/button";
 import { BaseDialog } from "@/components/dialog/dialog";
 import { Form, Input, InputGroup, InputLabel } from "@/components/input";
 import { PublicTimeEntryWithTag } from "@/types/client";
-import { FC, SetStateAction } from "react";
+import { FC } from "react";
 
 interface TimeEntryFormDialogProps {
 	isOpen: boolean;
@@ -23,36 +23,44 @@ export const TimeEntryFormDialog: FC<TimeEntryFormDialogProps> = ({
 			size="lg"
 			buttons={
 				<>
-					<Button variant="outline">Cancel</Button>
+					<Button variant="outline" onClick={() => setIsOpen(false)}>
+						Cancel
+					</Button>
 					<Button>{entry ? "Save" : "Add"}</Button>
 				</>
 			}
 		>
 			<Form>
 				<InputGroup>
-					<InputLabel text="Task Name" />
-					<Input />
+					<InputLabel text="Task Name" required />
+					<Input
+						placeholder={
+							entry
+								? "What were you working on?"
+								: "What did you work on?"
+						}
+					/>
 				</InputGroup>
 
 				<div className="flex gap-2">
 					<InputGroup>
-						<InputLabel text="Start Date" />
-						<Input />
+						<InputLabel text="Start Date" required />
+						<Input type="date" />
 					</InputGroup>
 					<InputGroup>
-						<InputLabel text="Start Time" />
-						<Input />
+						<InputLabel text="Start Time" required />
+						<Input type="time" />
 					</InputGroup>
 				</div>
 
 				<div className="flex gap-2">
 					<InputGroup>
-						<InputLabel text="End Date" />
-						<Input />
+						<InputLabel text="End Date" required />
+						<Input type="date" />
 					</InputGroup>
 					<InputGroup>
-						<InputLabel text="End Time" />
-						<Input />
+						<InputLabel text="End Time" required />
+						<Input type="time" />
 					</InputGroup>
 				</div>
 
--- src/components/input/input-label.tsx
+++ src/components/input/input-label.tsx
@@ -1,16 +1,16 @@
 interface InputLabel {
-    text: string;
-    required?: boolean;
+	text: string;
+	required?: boolean;
 }
 
 export const InputLabel: React.FC<InputLabel> = ({
-    text,
-    required = false,
+	text,
+	required = false,
 }) => {
-    return (
-        <p className="">
-            {text}
-            {required && <span className="ml-1 text-red-500">*</span>}
-        </p>
-    );
+	return (
+		<p className="text-sm text-gray-900">
+			{text}
+			{required && <span className="ml-1 text-red-500">*</span>}
+		</p>
+	);
 };
