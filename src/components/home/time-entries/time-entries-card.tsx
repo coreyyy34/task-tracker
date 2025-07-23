2f038ddf49fe3971797c3a131b29e815cd79a0b3
Corey <cbh344@outlook.com>
2025-07-23 19:38:25 +1200

    add filters and add entry button

--- src/components/home/time-entries/time-entries-card.tsx
+++ src/components/home/time-entries/time-entries-card.tsx
@@ -1,11 +1,25 @@
-import { Logs } from "lucide-react";
+import { Filter, Logs, Plus } from "lucide-react";
 import { Card, CardTitle } from "../../card";
 import { TimeEntriesList } from "./time-entries-list";
+import { Button } from "@/components/common/button";
 
 const TimeEntriesCard = () => {
 	return (
 		<Card>
-			<CardTitle title="Time Entries" icon={Logs} />
+			<div className="flex items-center justify-between">
+				<CardTitle title="Time Entries" icon={Logs} />
+				<div className="space-x-2">
+					<Button variant="outline">
+						<Filter />
+						Filters
+					</Button>
+					<Button>
+						<Plus />
+						Add entry
+					</Button>
+				</div>
+			</div>
+
 			<TimeEntriesList />
 		</Card>
 	);
