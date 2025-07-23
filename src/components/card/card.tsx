14b890374788016827008a6d1a745ac16789b5ba
Corey <cbh344@outlook.com>
2025-07-24 09:56:00 +1200

    add filters ui to time entries card

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -2,7 +2,7 @@ import { FC, PropsWithChildren } from "react";
 
 export const Card: FC<PropsWithChildren> = ({ children }) => {
 	return (
-		<div className="rounded-lg border-2 p-4 bg-neutral-100/25 border-gray-200 shadow-sm space-y-4">
+		<div className="rounded-lg border-2 p-4 bg-white border-gray-300 shadow-sm space-y-4">
 			{children}
 		</div>
 	);
--- src/components/home/time-entries/time-entries-card.tsx
+++ src/components/home/time-entries/time-entries-card.tsx
@@ -5,8 +5,10 @@ import { Button } from "@/components/common/button";
 import { useState } from "react";
 import { TimeEntryFormDialog } from "./time-entry-form-dialog";
 import { PublicTimeEntryWithTag } from "@/types/client";
+import { Input, InputGroup, InputLabel } from "@/components/input";
 
 const TimeEntriesCard = () => {
+	const [isFiltersOpen, setFiltersOpen] = useState(false);
 	const [isFormOpen, setFormOpen] = useState(false);
 	const [editingEntry, setEditingEntry] = useState<
 		PublicTimeEntryWithTag | undefined
@@ -27,7 +29,10 @@ const TimeEntriesCard = () => {
 			<div className="flex items-center justify-between">
 				<CardTitle title="Time Entries" icon={Logs} />
 				<div className="space-x-2">
-					<Button variant="outline">
+					<Button
+						variant="outline"
+						onClick={() => setFiltersOpen((prev) => !prev)}
+					>
 						<Filter />
 						Filters
 					</Button>
@@ -35,6 +40,32 @@ const TimeEntriesCard = () => {
 						<Plus />
 						Add entry
 					</Button>
+				</div>
+			</div>
+
+			<div
+				className={`grid transition-all duration-500 ${
+					isFiltersOpen ? "grid-rows-[1fr]" : "grid-rows-[0fr]"
+				}`}
+			>
+				<div className="overflow-hidden">
+					<div className="p-4 bg-gray-50 rounded-lg space-y-4">
+						<div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+							<InputGroup>
+								<InputLabel text="Filter by Tag" />
+								<Input />
+							</InputGroup>
+							<InputGroup>
+								<InputLabel text="Filter by Date" />
+								<Input type="date" />
+							</InputGroup>
+						</div>
+						<div className="flex gap-2">
+							<Button variant="outline" size="sm">
+								Clear Filters
+							</Button>
+						</div>
+					</div>
 				</div>
 			</div>
 
